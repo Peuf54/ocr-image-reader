5bee300e431456d7a38801ad022fb72bed4d6490
Hugo Petament <h.petament@icloud.com>
2023-07-22 12:34:00 +0200

    Setup styles structure, add main gems cloudinary, dotenv, sassc-rails. Pinning tesseractJS

---
+++ config/importmap.rb
@@ -0,0 +1,12 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tesseract.js", to: "https://ga.jspm.io/npm:tesseract.js@4.1.1/src/index.js"
+pin "#src/worker/node/index.js", to: "https://ga.jspm.io/npm:tesseract.js@4.1.1/src/worker/browser/index.js"
+pin "is-electron", to: "https://ga.jspm.io/npm:is-electron@2.2.2/index.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
+pin "regenerator-runtime/runtime", to: "https://ga.jspm.io/npm:regenerator-runtime@0.13.11/runtime.js"
