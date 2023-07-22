99b84360969e4a8df623985b5cbc461096276164
Hugo Petament <h.petament@icloud.com>
2023-07-22 18:41:57 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.desstroy_all
+User.destroy_all
 
 User.create!({email: "test@test.com", password: "password"})
 User.create!({email: "test2@test.com", password: "password"})
